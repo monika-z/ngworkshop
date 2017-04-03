50ea57eca35e1a9eb7be4b748ed53a22e1ebc69a
Monika Zygmunt <monika.a.zygmunt@gmail.com>
2017-04-03 19:54:02 +0000

    travis fix

--- db/schema.rb
+++ db/schema.rb
@@ -61,8 +61,8 @@ ActiveRecord::Schema.define(version: 20170403192705) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
-  
-    create_table "users", force: :cascade do |t|
+
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -79,8 +79,6 @@ ActiveRecord::Schema.define(version: 20170403192705) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-
 
   add_foreign_key "participations", "students"
   add_foreign_key "participations", "subject_items"
